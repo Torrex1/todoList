887e3b82a6638432d772be6733e4d577e4404b28
Torrex1 <flendik666@gmail.com>
2025-04-27 22:30:22 +0300

    add header & todo form on home page

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import Header from '../components/Header.jsx'
+import TodoForm from "../components/TodoForm.jsx";
+
+export default function HomePage() {
+    return (
+        <>
+            <Header />
+            <TodoForm />
+        </>
+    )
+}
