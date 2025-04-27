ba56c94945f4b187ff949141c93772c25a7465f5
Torrex1 <flendik666@gmail.com>
2025-04-27 23:54:04 +0300

    add all components displayed on the home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,26 @@
-import Header from '../components/Header.jsx'
-import TodoForm from "../components/TodoForm.jsx";
+import '../styles/homePage.css'
 
-export default function HomePage() {
+import Header from '../components/Header.jsx'
+import TodoForm from "../components/TodoForm/TodoForm.jsx";
+import TodoList from "../components/TodoList.jsx";
+
+export default function HomePage({onAdd ,todos, onToggle, onDelete}) {
     return (
         <>
-            <Header />
-            <TodoForm />
+            <div className='container'>
+                <Header />
+                <TodoForm
+                    onAdd={onAdd}
+                />
+
+                <div className="container__todos">
+                    <TodoList
+                        todos={todos}
+                        onDelete={onDelete}
+                        onToggle={onToggle}
+                    />
+                </div>
+            </div>
         </>
     )
 }
