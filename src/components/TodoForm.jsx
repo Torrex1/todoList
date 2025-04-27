ac80aa4f5a73546523a3770946f8604cdea7a2eb
Torrex1 <flendik666@gmail.com>
2025-04-27 22:29:58 +0300

    add todo form layout

---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,24 @@
+export default function TodoForm () {
+    return (
+        <form className="form-todo">
+            <div className="todo-task">
+                <input
+                    className='todo-task__text'
+                    type="text"
+                    placeholder="Что нужно сделать?"
+                />
+
+                <button type='submit'>
+                    Добавить
+                </button>
+            </div>
+
+            <div className="todo-deadline">
+                <input
+                    type="text"
+                    placeholder="dd.mm"
+                />
+            </div>
+        </form>
+    )
+}
