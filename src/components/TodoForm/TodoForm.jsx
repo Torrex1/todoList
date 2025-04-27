91c6cb2c266d73d28b1da5f36d084212c6a8a02a
Torrex1 <flendik666@gmail.com>
2025-04-27 23:25:10 +0300

    add form submit handler

--- src/components/TodoForm/TodoForm.jsx
+++ src/components/TodoForm/TodoForm.jsx
@@ -1,13 +1,28 @@
 import './todoform.css'
+import {useState} from "react";
 
-export default function TodoForm () {
+export default function TodoForm ({onAdd}) {
+    const [text, setText] = useState('')
+    const [deadline, setDeadline] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (text.trim() && deadline.trim()) {
+            onAdd(text, deadline)
+            setText('')
+            setDeadline('')
+        }
+    }
+
     return (
-        <form className="form-todo">
+        <form className="form-todo" onSubmit={handleSubmit}>
             <div className="todo-task">
                 <input
                     className='todo-task__text'
                     type="text"
                     placeholder="Что нужно сделать?"
+                    value={text}
+                    onChange={e => setText(e.target.value)}
                 />
 
                 <button type='submit'>
@@ -19,6 +34,8 @@ export default function TodoForm () {
                 <input
                     type="text"
                     placeholder="dd.mm"
+                    value={deadline}
+                    onChange={e => setDeadline(e.target.value)}
                 />
             </div>
         </form>
