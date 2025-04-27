078b6d895bf225b3624ce36bc0670981911574d0
Torrex1 <flendik666@gmail.com>
2025-04-27 23:49:15 +0300

    create add new todo func

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import HomePage from './pages/HomePage.jsx'
 
+import { useState } from "react";
+
 export default function App () {
+    const [todos, setTodos] = useState([])
+
+    const addTodo = (text, deadline) => {
+        const newTodo = {
+            id: Date.now(),
+            text: text,
+            deadline: deadline,
+            completed: false
+        }
+        setTodos([newTodo, ...todos]); //todo поменять местами
+        console.log(newTodo);
+    }
     return (
         <HomePage />
+        <HomePage
+            onAdd={addTodo}
+            todos={todos}
+        />
     )
 }
