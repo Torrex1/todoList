987a70079721d7e37656751df5392323413f5c09
Torrex1 <flendik666@gmail.com>
2025-04-27 23:55:24 +0300

    add props-func into component

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,11 +1,13 @@
 import TodoItem from "./TodoItem/TodoItem.jsx";
 
-export default function TodoList ({todos}) {
+export default function TodoList ({todos, onToggle, onDelete}) {
     return (
         todos.map((todo) => (
             <TodoItem
                 key={todo.id}
                 todo={todo}
+                onToggle={onToggle}
+                onDelete={onDelete}
             />
         ))
     )
