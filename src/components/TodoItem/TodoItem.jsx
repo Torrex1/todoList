45b25c0ca509299c1fc3334aae8ae32c8ba3491f
Torrex1 <flendik666@gmail.com>
2025-04-27 23:55:49 +0300

    init props and using it on layout

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -1,22 +1,23 @@
 import './todoitem.css'
 
-export default function TodoItem () {
+export default function TodoItem ({todo, onToggle, onDelete}) {
     return (
         <div className='todo-item'>
-            <div className='todo-item__date'>
+            <div className='todo-item__deadline'>
+                <span>{todo.deadline}</span>
             </div>
 
             <div className='todo-item__text'>
-                <div></div>
+                <div>{todo.text}</div>
             </div>
 
             <div className='todo-item__icons'>
-                <div>
-                    <img src="src/assets/icons/uncompleteIcon.svg" alt="uncompleteIcon"/>
-                    <img src="src/assets/icons/completeIcon.svg" alt="completeIcon"/>
+                <div onClick={() => onToggle(todo.id)}>
+                    <img style={todo.completed === false? {display: 'block'} : {display: 'none'}} src="src/assets/icons/uncompleteIcon.svg" alt="uncompleteIcon"/>
+                    <img style={todo.completed ? {display: 'block'} : {display: 'none'}} src="src/assets/icons/completeIcon.svg" alt="completeIcon"/>
                 </div>
 
-                <div >
+                <div onClick={() => onDelete(todo.id)}>
                     <img src="src/assets/icons/deleteIcon.svg" alt="deleteIcon"/>
                 </div>
             </div>
